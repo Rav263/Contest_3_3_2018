af5c9bebfdc6f770a0c6d865ec621871eb2b72fb
RAV263 <nickiforov.nik@gmail.com>
2018-09-30 16:09:53 +0300

    Добавлена претензия к принимающим

--- D.c
+++ D.c
@@ -28,7 +28,12 @@ main(void)
     memset(out_array, 0, NUM_OF_BYTES);
 
     for (int j = 0; scanf("%hhx", &now) == 1; j++, j %= NUM_OF_BYTES) {
-        for (int i = 0; i < CHAR_BIT; i++) {
+        /* So, in the last reject, you asked me to use "not for".
+         * I don't know why. "For" is beautifull and amazing. 
+         * Here i need to use counter variable. And if i will use while
+         * it will be local variable for all programm, it's not good;
+         */
+        for (int i = 0; i < NUM_OF_BYTES; i++) {
             out_array[i] += (now & 1) * MASK_ILON;
             
             now >>= 1;
